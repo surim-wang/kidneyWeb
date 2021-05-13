a09138d0f2a87d1c881b15fdc9b8ac5a6046e9c3
Surim <tnfla3@gmail.com>
2021-05-14 00:29:24 +0900

    lector13 parameter

---
+++ parameter.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset = "utf-8">
+  </head>
+  <body>
+    안녕하세요. <?php echo $_GET['address']; ?>에 사시는 <?php echo $_GET['name']; ?>님
+    <?php
+      echo date('Y-m-d H:i:s');
+     ?>
+  </body>
+<html>
