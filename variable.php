bd310df244c198869b998afb7378d057b540bb05
Surim <tnfla3@gmail.com>
2021-05-14 00:18:38 +0900

    lector12 variable

---
+++ variable.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "leezche";
+    echo "Lorem ipsum dolor sit amet, ".$name."   consectetur adipisicing elit, sed do eiusmod tempor ".$name." incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate ".$name." velit esse cillum dolore eu fugiat nulla pariatur. Excepteur egoing sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+     ?>
+  </body>
+</html>
