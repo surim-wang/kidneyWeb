dba7055ad7e4c00982cd3a36693872e876805eb2
Surim <tnfla3@gmail.com>
2021-05-14 00:07:12 +0900

    lector 11 strings

---
+++ strings.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<body>
+  <h1>String & String Operator</h1>
+<?php
+echo "Hello \"w\"ord"
+ ?>
+ <h2>concatentaion Operator</h2>
+ <?php
+ echo "Hello "."world"
+  ?>
+  <h2>String length function</h2>
+  <?php
+  echo strlen("Hello world")
+   ?>
+</body>
+</html>
