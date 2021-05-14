1a72e76865827744d541b3762037140bd8fc9770
Surim <tnfla3@gmail.com>
2021-05-15 00:30:23 +0900

    lector20 conditional

---
+++ conditional.php
@@ -0,0 +1,20 @@
+<!doctype HTML>
+<html>
+  <head>
+    <meta charset="utf-i">
+
+  </head>
+  <body>
+    <h1>Conditional</h1>
+    <h2>if</h2>
+    <?php
+    echo '1<br>';
+    if (true){
+      echo '2-1<br>';
+    } else{
+      echo '2-2<br>';
+    }
+    echo '3<br>';
+    ?>
+  </body>
+</html>
