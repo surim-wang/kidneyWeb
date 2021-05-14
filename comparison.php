3f886c9b4866299f59a2a9989db0a21d10dd8013
Surim <tnfla3@gmail.com>
2021-05-15 00:22:43 +0900

    lector19 comparison

---
+++ comparison.php
@@ -0,0 +1,16 @@
+<!doctype HTML>
+<html>
+  <head>
+    <meta charset="utf-i">
+
+  </head>
+  <body>
+    <h1>Comparison Operators &amp;
+    Boolean data type</h1>
+  <?php var_dump(1+1);?><br>
+  <?php var_dump("2");?><br>
+  <?php var_dump(1==1);?><br>
+  <?php var_dump(2>1);?><br>
+  <?php var_dump(1>=1);?><br>
+  </body>
+</html>
